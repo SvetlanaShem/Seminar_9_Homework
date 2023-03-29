7aea934ea6427659f78a9012f7c646c752a68e9f
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-30 00:32:03 +0300

    Задание 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int m = GetNum("Введите натуральное число: ", "Ошибка ввода!");
+int n = GetNum("Введите натуральное число: ", "Ошибка ввода!");
+int result = GetSum(m, n);
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {result}");
+
+int GetNum(string message, string error)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect && num > 0) return num;
+        else Console.WriteLine(error);
+    }
+}
+int GetSum(int a, int b)
+{
+    if (a == b)
+        return b;
+    return (a + GetSum(a + 1, b));
+}
