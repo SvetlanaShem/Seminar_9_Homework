6b869837cdcffd76cc9372f00eb4ad75d57ec3ec
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-30 00:49:22 +0300

    Задание 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int m = GetNum("Введите натуральное число: ", "Ошибка ввода!");
+int n = GetNum("Введите натуральное число: ", "Ошибка ввода!");
+int result = AkkermanFunction(m, n);
+Console.WriteLine($"Результат вычисления функции Аккермана равен {result}");
+
+int GetNum(string message, string error)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect && num > 0) return num;
+        else Console.WriteLine(error);
+    }
+}
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return AkkermanFunction(m - 1, 1);
+    else return (AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
+}
