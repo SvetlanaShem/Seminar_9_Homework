3011da84e40d8624e74e4fd1f5a51ae44beaf1c7
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-30 00:13:10 +0300

    Задание 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int n = GetNum("Введите натуральное число: ", "Ошибка ввода!");
+string result = PrintDigits(n);
+Console.WriteLine(result);
+
+int GetNum(string message, string error)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect && num > 0) return num;
+        else Console.WriteLine(error);
+    }
+}
+
+string PrintDigits(int a)
+{
+    if (a == 1)
+        return 1.ToString();
+    return (a + "," + PrintDigits(a - 1));
+}
